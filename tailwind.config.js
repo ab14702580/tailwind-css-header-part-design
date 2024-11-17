5faca905e089db9798b77c1d7943e24287d46163
ab14702580 <abdullahfahad14701470@gmail.com>
2024-11-17 23:23:19 +0600

    full page commplite

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,25 @@ module.exports = {
       fontFamily: {
         roboto: ["Roboto","sans-serif"],
       },
+      colors: {
+        'm-black': '#161313',
+        'orange-m': '#E34D00',
+        'b-gradient': '#1E1C1C',
+        'content-color': '#BCBCBC',
+      },
+      keyframes: {
+        'infinit-animate':{
+          '0%':{transform: 'translateX(-.2%)'},
+          '25%': {transform: 'translateX(-.2%)'},
+          '45%': {transform: 'translateX(-50%)'},
+          '65%': {transform: 'translateX(-50%)'},
+          '85%': {transform: 'translateX(-100%)'},
+          '100%':{transform: 'translateX(-100%)'},
+        },
+      },
+      animation: {
+        'infinit-animate': 'infinit-animate 10s linear infinite'
+      },
     },
   },
   plugins: [],
